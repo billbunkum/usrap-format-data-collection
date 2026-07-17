4697defe606941b016f5842a348df9f229178d2e
Bill Bunkum <20425559+billbunkum@users.noreply.github.com>
2026-07-17 19:21:48 -0400

    reorder comments

--- format-dc-v2.3.4--beta.py
+++ format-dc-v2.3.4--beta.py
@@ -6,34 +6,18 @@
 #############################################################################
 
 '''
-- Still need to know what file to run against and which Cols/format to export:
-    Think it will be the "spatial" file with combined Batch and ArcGIS Cols
-- V2.1 should take this one file and parse out all needed info., format, and export into proper SR4D format.
-- V2.1.1 Begins logging missing cells but does not discriminate yet.
-- V2.1.2 minor differences to startup and error messages
-- V2.1.3 creates .csv of rows with MISSING data, only Cols that 'matter', e.g. not Comments.
-- V2.2 will provide a “log file” with Rows that ONLY have REQUIRED missing data.
-- V2.2.1 Fixes 'Speed limit' logic
-- V2.2.2 Fixes 'Number of Lanes' logic
-- V2.2.3 Start changes to workflow & features, i.e. Options '1' and '2'.
-  - DID NOT DO THIS - Removes references to 'sr4d'
-    - NOTE_: present Functions have if/else concerning `file_format` == 'sr4d'; this is DEPRECATED.
-    - Begins internal work for 2.2.4, testing 
-  - Option '1' to CHECK Spatial for Missing
-    - USAGE_: Outputs a single Missing Log .csv
-      - OUTPUT-ConvertSpatial--someFilename-MissingCellsLog.csv
-    - User then needs to correct Missing Cells within a Spatial file
-  - Option '2' to 'CONVERT Spatial to ViDA'
-    - USAGE_: Outputs two .csv; 1 Missing Log, 1 CSV prepared for ViDA upload
-    - will take a Spatial .csv as INPUT and OUTPUT a someFile.csv
-    - if Missing Cells (in necessary Rows), will OUTPUT a 'Missing Cell Log' file + Warning Message (ViDA RPS will fail. Missing Rows. See Log file.)
-- v2.3 Add workflow Feature, i.e. adds Option '3'.
-  - Option '3' to 'CLEAN Spatial'
-    - Removes unneeded Cols from Spatial file.
-      - Workflow requires User to work from a Spatial file to make corrections, e.g. missing cells, incorrect data, etc., *not* a ViDA file.
-- V2.3.1 Adds Option '4' to create a CSV with only Rows with Missing Cells
-  - Only check for necessary Cols
-  - Works with other options EXCEPT for Option '1' Check Spatial for Missing Cells
+- V2.3.4
+  - [X] Fix Col name Errors shown by ViDA
+  - [X] Deal with Col names ViDA claims need values - might be User error with ViDA site in choosing Dataset
+  - [] Correct Col 38 'Intersecting Road Volume' - add ViDA calc (we missed)
+  - [] Add Option 5 'Strip Missing Rows for ViDA'
+    - IDEA 1: read in 'missing-only' CSV as a 2nd batch();  a DEF uses the 'Image reference' to create a Mask; remove these Rows and OUTPUT a 'stripped' CSV
+    - IDEA 2: add onto `whitelist_cols()` with a `file_format` IF/ELSE clause; create a Mask with `.isna()` based on Whitelisted `keys`
+  - [] SETUP needs REFACTOR into various DEF calls --> TBD
+
+- V2.3.3 Fixing MISSING LOG
+  - [X] Should include RT_UNIQUE ( Road Name)
+  - [X] Should include Image Reference
 
 - V2.3.2 Addresses issues found during first real test with 056-spatial.csv
   - [X] Might need dummy info for:
@@ -49,18 +33,40 @@
       [X] FIX 2: Auto-decide a DEFAULT value (ASK ALEX ON THIS) # ANCHOR / WORKING: Need Instruction
         Currently, default is 'Urban' if Field is not 'Rural'
       [X] ASK ALEX ABOUT FIX 2
-  - [] SETUP needs REFACTOR into various DEF calls --> TBD
+  - [] _pushed_ SETUP needs REFACTOR into various DEF calls --> TBD
 
-- V2.3.3 Fixing MISSING LOG
-  - [X] Should include RT_UNIQUE ( Road Name)
-  - [X] Should include Image Reference
+- V2.3.1 Adds Option '4' to create a CSV with only Rows with Missing Cells
+  - Only check for necessary Cols
+  - Works with other options EXCEPT for Option '1' Check Spatial for Missing Cells
 
-- V2.3.4
-  - [X] Fix Col name Errors shown by ViDA
-  - [] Deal with Col names ViDA claims need values - might be User error with ViDA site in choosing Dataset
-  - [] Add Option 5 'Strip Missing Rows for ViDA'
-    - IDEA 1: read in 'missing-only' CSV as a 2nd batch();  a DEF uses the 'Image reference' to create a Mask; remove these Rows and OUTPUT a 'stripped' CSV
-    - IDEA 2: add onto `whitelist_cols()` with a `file_format` IF/ELSE clause; create a Mask with `.isna()` based on Whitelisted `keys`
+- v2.3 Add workflow Feature, i.e. adds Option '3'.
+  - Option '3' to 'CLEAN Spatial'
+    - Removes unneeded Cols from Spatial file.
+      - Workflow requires User to work from a Spatial file to make corrections, e.g. missing cells, incorrect data, etc., *not* a ViDA file.
+
+- V2.2.3 Start changes to workflow & features, i.e. Options '1' and '2'.
+  - DID NOT DO THIS - Removes references to 'sr4d'
+    - NOTE_: present Functions have if/else concerning `file_format` == 'sr4d'; this is DEPRECATED.
+    - Begins internal work for 2.2.4, testing 
+  - Option '1' to CHECK Spatial for Missing
+    - USAGE_: Outputs a single Missing Log .csv
+      - OUTPUT-ConvertSpatial--someFilename-MissingCellsLog.csv
+    - User then needs to correct Missing Cells within a Spatial file
+  - Option '2' to 'CONVERT Spatial to ViDA'
+    - USAGE_: Outputs two .csv; 1 Missing Log, 1 CSV prepared for ViDA upload
+    - will take a Spatial .csv as INPUT and OUTPUT a someFile.csv
+    - if Missing Cells (in necessary Rows), will OUTPUT a 'Missing Cell Log' file + Warning Message (ViDA RPS will fail. Missing Rows. See Log file.)
+
+- V2.2.2 Fixes 'Number of Lanes' logic
+- V2.2.1 Fixes 'Speed limit' logic
+- V2.2 will provide a “log file” with Rows that ONLY have REQUIRED missing data.
+- V2.1.3 creates .csv of rows with MISSING data, only Cols that 'matter', e.g. not Comments.
+- V2.1.2 minor differences to startup and error messages
+- V2.1.1 Begins logging missing cells but does not discriminate yet.
+- V2.1 should take this one file and parse out all needed info., format, and export into proper SR4D format.
+- Still need to know what file to run against and which Cols/format to export:
+    Think it will be the "spatial" file with combined Batch and ArcGIS Cols
+
 '''
 
 #############################################################################
@@ -950,7 +956,7 @@ def create_missing_csv(): # ANCHOR / WORKING // Does this need a `blacklist`??? 
 
   return missing_only_df
 
-def strip_missing():
+def strip_missing(): # // ANCHOR // WORKING
   input_df = batch.copy()
   mask = input_df[valid_keys].isna().any(axis=1)
 
